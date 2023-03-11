e5cb045878f4cfc933591682523caab99f68f6ea
ronan <ronanmahtolia10@gmail.com>
2023-03-12 06:28:19 +0800

    going to slep

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'success' : '',
+        'error' : '',
+        'warning' : '',
+      }
+    },
   },
   plugins: [],
 }
