3d36b550b3866357671f46d2e44966dcc106dd18
ronan <ronanmahtolia10@gmail.com>
2023-03-13 07:22:01 +0800

    Finish upload

--- src/components/Other/UploadFileButton.js
+++ src/components/Other/UploadFileButton.js
@@ -1,21 +1,13 @@
 import React, { useRef } from 'react'
 import UploadFileIcon from '@mui/icons-material/UploadFile';
 
-function UploadFileButton({ onClick }) {
-    const color = "#109CF1"
+function UploadFileButton() {
     const inputRef = useRef()
-    console.log("Render")
-    // var selected = document.getElementById("uploadBox").files.length > 0;
-    // if(selected){
-    //     onClick()
-    // }
+
     return (
         <label className={`bg-[#109CF1] text-center text-white rounded-md px-2 py-2 shadow-[#109CF1] shadow-md w-1/3`}>
-            Upload Files <UploadFileIcon />
-            <form onSubmit={() => console.log("Form")}>
+            Choose Files <UploadFileIcon />
                 <input ref={inputRef}  id="uploadBox" type="file" multiple className='bg-transparent mt-4 hidden' />
-                <button type='submit' > Submit</button>
-            </form>
         </label>
     )
 }
--- src/pages/UploadEmail.js
+++ src/pages/UploadEmail.js
@@ -10,7 +10,7 @@ function UploadEmail() {
     const handleUpload = () => {
         setSubmit(false)
         setTimeout(() => {
-            navigate("/Dashboard")
+            navigate("/app")
         }, 4500)
     }
     return (
@@ -18,10 +18,16 @@ function UploadEmail() {
             <div className='flex flex-col items-center'>
                 <img src={logo} className='w-1/2 h-1/5'></img>
                 <UploadFileButton onClick={handleUpload} className="w-1/2" />
+
+                <div className="mt-8" onClick={handleUpload}>
+                <label className={`bg-[#109CF1] text-center text-white rounded-md px-2 py-2 shadow-[#109CF1] shadow-md w-1/3`}>
+                    Upload Files 
+                </label>
+                </div>
             </div>
             :
             <div className='flex justify-center items-center h-screen'>
-                <CircularProgress size='lg' className="h-full w-full"/>
+                <CircularProgress size='lg' className="h-full w-full" />
             </div>
     )
 }
